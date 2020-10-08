6142df65889d4acd6858af4f41827e805362667e
kei-cell <k_shintani@kei-creation.main.jp>
2020-10-08 12:55:10 +0900

    no message

--- HelloController.java
+++ HelloController.java
@@ -1,11 +1,54 @@
-package com.example.demo.controller;
+package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-
+@RestController
 public class HelloController {
-    @GetMapping
-    public String getHello() {
-        return "hello";
-    }
+        String[] names = {"tuyano","hanako","taro","sachiko","ichiro"};
+        String[] mails = {"syoda@tuuyano.com","hanako@flower","taro@yamada","sachiko@happy","ichiro@baseball"};
+
+        @RequestMapping("/")
+        public ModelAndView index(ModelAndView mav){
+            mav.setViewName("index");
+            mav.addObject("msg","current data.");
+            DataObject obj = new DataObject(123,"小泉進次郎","hanako@flower");
+            mav.addObject("object",obj);
+            return mav;
+
+        }
 }
+class DataObject{
+    private int id;
+    private String name;
+    private String value;
+
+
+public DataObject(int id,String name,String value){
+    super();
+    this.id = id;
+    this.name = name;
+    this.value = value;
+}
+
+public int getId(){
+    return id;
+}
+public void setId(int id){
+    this.id = id;
+}
+public String getName(){
+    return name;
+}
+public void setName(String name){
+    this.name = name;
+}
+public String getValue(){
+    return value;
+}
+public void setValue(String value){
+        this.value= value;
+}
+}
+
